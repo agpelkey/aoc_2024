0fa70634ce47296e7f746a71c6ae9448d691d6c5
alex <agpelkey@yahoo.com>
2025-01-08 22:43:34 -0500

    got wrecked, felt cute, might post again

--- day1/main.go
+++ day1/main.go
@@ -3,14 +3,33 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func split_list(s string) []string {
-	result := strings.Split(s, "\n")
+func split_list(lines string) ([]int, []int) {
 
-	fmt.Println(result)
-	return result
+	//fmt.Println(result)
+	//return result
+	list1 := []int{}
+	list2 := []int{}
+	for _, line := range lines {
+		result := strings.Split(lines, "\n")
+		//result := strings.Split(line, "\n")
+		//fmt.Println(line)
+		first_num, err := strconv.Atoi(result[0])
+		if err != nil {
+			fmt.Println(err)
+		}
+		second_num, err := strconv.Atoi(result[1])
+		if err != nil {
+			fmt.Println(err)
+		}
+		list1 = append(list1, first_num)
+		list2 = append(list2, second_num)
+	}
+
+	return list1, list2
 }
 
 //go:embed input.txt
@@ -25,7 +44,7 @@ var input string
 func main() {
 
 	// Get the first column from the list
-	split_list(input)
+	fmt.Println(split_list(input))
 
 	// Get the second column from the list
 
